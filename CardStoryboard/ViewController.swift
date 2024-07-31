4926ff272bb629d93249392ffa03cbc7e908f17a
Zehra Nur Acik <zehranuracik@hotmail.com>
2024-07-31 21:32:44 +0300

    Card

--- CardStoryboard/ViewController.swift
+++ CardStoryboard/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    var timer : Timer!
+    var cards : [UIImage] = Deck.allValues
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startTimer()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
+    }
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage(){
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
     }
 
-
+    @IBAction func stopButtonClicked(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func restartButtonClicked(_ sender: Any) {
+        timer.invalidate()
+        startTimer()
+    }
+    
 }
 
